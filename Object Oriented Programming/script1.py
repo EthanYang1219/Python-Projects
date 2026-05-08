7cbfa6bbf19629eef3354ffdaccdee3fee41cd7d
EthanYang1219 <yangethan2006@gmail.com>
2026-05-08 11:58:24 -0700

    Add new examples for control flow, modules, and variable scope; implement functions for area and circumference calculations

---
+++ Object Oriented Programming/script1.py
@@ -0,0 +1,11 @@
+def favourite_food(food):
+    print(f"Your favourite food is {food}")
+
+
+def main():
+    print("This is script1")
+    favourite_food("steak")
+    print("BYEBYE")
+
+if __name__ == '__main__':
+    main()
